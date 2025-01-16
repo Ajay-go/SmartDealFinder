4eb99a23d99da27afba628ca10f784d2d752bc89
AJAY GOUR <ajaygour3333@gmail.com>
2025-01-16 20:15:19 +0530

    completed functionality for flipkart

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+## getting samsung models
+
 from bs4 import BeautifulSoup
 
 with open('C:/Users/ajayg/OneDrive/Desktop/pro2/SmartDealFinder/models.html', 'r', encoding='utf-8') as file:
@@ -13,11 +15,8 @@ samsung_list = []
 for name in model_names:
     samsung_list.append(name)
 
-# print(samsung_list)
 
-# print()
-
-############################################################################
+######################################################################## apple models ########
 
 from bs4 import BeautifulSoup
 
@@ -40,7 +39,7 @@ for name in model_names:
 
 print()
 
-########################################################################
+######################################################################## oneplus models ########
 oneplus_list = []
 
 with open('C:/Users/ajayg/OneDrive/Desktop/pro2/SmartDealFinder/oneplus_models.html','r', encoding='utf-8') as file:
@@ -53,13 +52,21 @@ for i in soup.find_all('div', class_ = 'flex flex-col items-center justify-cente
     oneplus_list.append(i.find('span', class_ = 'caption text-center line-clamp-3').text.strip())
 
 
-# print(oneplus_list)    
+#########################################################
+### importing scrape function from scrape prices file
 
+from scrape_prices import scrape_prices_flipkart
+
+from scrape_prices import scrape_prices_amazon
 
 #########################################################
+####### dashboard ########
+
+
 
 import streamlit as st 
-All_data = {
+All_data = { #### all models list for corresponding brands
+    
     'samsung': samsung_list,
     'apple': apple_list,
     'oneplus': oneplus_list
@@ -67,31 +74,41 @@ All_data = {
 
 st.title("Smart Deal Finder")
 
-# Dropdown for selecting brand
+##### Dropdown for selecting brand
 brands = ['samsung', 'apple', 'oneplus']
 selected_brand = st.selectbox("Select Brand", brands)
 
-# Dropdown for selecting model based on the selected brand
+##### Dropdown for selecting model based on the selected brand
 selected_model = st.selectbox("Select Model", All_data[selected_brand])
 
-# Dropdown for RAM
+##### Dropdown for RAM
 ram_options = [4, 6, 8, 12]
 selected_ram = st.selectbox("Select RAM", ram_options)
 
-# Dropdown for ROM
-rom_options = [64, 128, 256, 512]  # Modify based on your data
+#### Dropdown for ROM
+rom_options = [64, 128, 256, 512] 
 selected_rom = st.selectbox("Select ROM", rom_options)
 
-butt = st.button('find prices')
+butt = st.button('find prices') ## button 
 
+## specification of target phone
+
+details = selected_model + " " + str(selected_ram) + "GB RAM " + str(selected_rom) + "GB ROM"
+
+## if button is pressed
 if(butt):
-    # price_at_flipkart = scrape_prices_flipkart()
-    # price_at_amazon = scrape_prices_amazon()
+
+    ## calling both functions 
+    
+    price_at_flipkart = scrape_prices_flipkart(details)
+    price_at_amazon = scrape_prices_amazon(details)
 
     col1,col2 = st.columns(2)
 
     with col1:
-        st.subheader("price at flipkart {}".format(100))
+        st.subheader("price at flipkart {}".format(price_at_flipkart))
     with col2:
-        st.subheader("price at amazon {}".format(1000))
+        st.subheader("price at amazon {}".format(price_at_amazon))
+
+
 
