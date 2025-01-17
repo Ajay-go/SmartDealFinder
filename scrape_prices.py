99e12817d7835c2717591d4a64fc3c4dae658f83
krishna-tony <tonystark00127@gmail.com>
2025-01-18 00:40:13 +0530

    add scrape_prize_amazon function

--- scrape_prices.py
+++ scrape_prices.py
@@ -36,11 +36,69 @@ def scrape_prices_flipkart(detail):
 
 def scrape_prices_amazon(detail):
 
-   ## complete it
-   return str(100)
+    driver.get('https://www.amazon.in/')
+    
+    try : # if captcha doesn't comes
+        amazon_search_box = driver.find_element('xpath', value= '/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/div/input')
+        amazon_search_box.send_keys(detail)
 
+        time.sleep(2)
 
-# //*[@id="container"]/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[2]/div[2]/div[1]/div[1]/div[1]
+        amazon_search_box.send_keys(Keys.ENTER)
+        time.sleep(5)
+        web_page = driver.page_source
+        
+        soup = BeautifulSoup(web_page, 'lxml')
 
-# <div class="Nx9bqj _4b5DiR">₹35,780</div>
-# <div class="Nx9bqj _4b5DiR">₹13,999</div>
+        price = "10"
+        for i in soup.find_all('div', class_ = 'sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16') : # if items comes in row wise fashion
+            price = i.find('span', class_ = 'a-offscreen').text.strip()
+            break
+            
+        if price == '10' : # if items comes in column wise fashion
+            for i in soup.find_all('div', class_ = 'sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20') :
+                price = i.find('span', class_ = 'a-offscreen').text.strip()
+                break
+            
+        time.sleep(5)
+        return price[1 :].replace(',', '')
+
+    except : # if captcha comes
+        
+        from amazoncaptcha import AmazonCaptcha
+        
+        link = driver.find_element('xpath', value= '/html/body/div/div[1]/div[3]/div/div/form/div[1]/div/div/div[1]/img').get_attribute('src')
+        
+        captcha = AmazonCaptcha.fromlink(link)
+        
+        ans = AmazonCaptcha.solve(captcha)
+        
+        input = driver.find_element('xpath', value= '/html/body/div/div[1]/div[3]/div/div/form/div[1]/div/div/div[2]/input')
+        input.send_keys(ans)
+        time.sleep(2)
+        input.send_keys(Keys.ENTER)
+        
+        amazon_search_box = driver.find_element('xpath', value= '/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/div/input')
+        amazon_search_box.send_keys(detail)
+
+        time.sleep(2)
+
+        amazon_search_box.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        web_page = driver.page_source
+            
+        soup = BeautifulSoup(web_page, 'lxml')
+
+        price = "10"
+        for i in soup.find_all('div', class_ = 'sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16') : # if items comes in row wise fashion
+            price = i.find('span', class_ = 'a-offscreen').text.strip()
+            break
+        
+        if price == '10' : # if items comes in column wise fashion
+            for i in soup.find_all('div', class_ = 'sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20') :
+                price = i.find('span', class_ = 'a-offscreen').text.strip()
+                break
+        
+        time.sleep(5)
+        return price[1 :].replace(',', '')
